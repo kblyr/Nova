bca78f6cbe28ca86fe79939548c714cbb0e8a1d5
John Josua Paderon <johnjosua.paderon@outlook.com>
2022-03-06 23:23:03 +0800

    Add StatusId

--- src/Identity/WebAPI/Schema/AddUser.cs
+++ src/Identity/WebAPI/Schema/AddUser.cs
@@ -2,7 +2,7 @@ namespace Nova.Identity.Schema;
 
 public static class AddUser
 {
-    public record Request(string Username, string Password);
+    public record Request(string Username, string Password, short StatusId);
 
     public record Response(int Id);
 }
