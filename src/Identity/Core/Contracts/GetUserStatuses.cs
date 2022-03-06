ddd6baa1b18bb22e34b295ffe66e035f861174a2
John Josua Paderon <johnjosua.paderon@outlook.com>
2022-03-06 23:52:31 +0800

    GetUserStatuses

---
+++ src/Identity/Core/Contracts/GetUserStatuses.cs
@@ -0,0 +1,11 @@
+namespace Nova.Identity.Contracts;
+
+public record GetUserStatuses : Request
+{
+    public record Response(IEnumerable<Response.UserStatusObj> UserStatuses) : Nova.Messaging.Response
+    {
+        public record UserStatusObj(short Id, string Name);
+    }
+
+    public static readonly GetUserStatuses Instance = new();
+}
---
+++ src/Identity/EFCore/Handlers/GetUserStatuses.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Nova.Identity.Handlers;
+
+sealed class GetUserStatuses_Handler : RequestHandler<GetUserStatuses>
+{
+    readonly IDbContextFactory<DatabaseContext> _contextFactory;
+
+    public GetUserStatuses_Handler(IDbContextFactory<DatabaseContext> contextFactory)
+    {
+        _contextFactory = contextFactory;
+    }
+
+    public async Task<Response> Handle(GetUserStatuses request, CancellationToken cancellationToken)
+    {
+        using var context = await _contextFactory.CreateDbContextAsync();
+        var query = context.UserStatuses.AsNoTracking()
+            .OrderBy(userStatus => userStatus.Name);
+        var userStatuses = await query
+            .Select(userStatus => new GetUserStatuses.Response.UserStatusObj(userStatus.Id, userStatus.Name))
+            .ToArrayAsync();
+
+        return new GetUserStatuses.Response(userStatuses);
+    }
+}
