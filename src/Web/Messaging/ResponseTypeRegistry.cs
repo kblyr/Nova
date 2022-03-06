2f3d978ba0a007886bac921ffe4d64ecac679632
John Josua Paderon <johnjosua.paderon@outlook.com>
2022-03-06 23:21:01 +0800

    ResponseTypeDefinitionMetadata

--- src/Web/Messaging/ResponseTypeRegistry.cs
+++ src/Web/Messaging/ResponseTypeRegistry.cs
@@ -1,5 +1,3 @@
-using System.Reflection.Metadata;
-
 namespace Nova.Messaging;
 
 public class ResponseTypeRegistry
@@ -16,14 +14,16 @@ public class ResponseTypeRegistry
         return ApiResponseFor(typeof(TResponse));
     }
 
-    public ResponseTypeRegistry Register<TResponse, TApiResponse>(int statusCode) where TResponse : Response
+    public ResponseTypeRegistry Register<TResponse, TApiResponse>(int statusCode, Action<ResponseTypeDefinitionMetadata>? configure = null) where TResponse : Response
     {
         var responseType = typeof(TResponse);
 
         if (_responseTypes.ContainsKey(responseType))
             return this;
-        
-        _responseTypes.Add(responseType, new(typeof(TApiResponse), statusCode));
+
+        var metadata = new ResponseTypeDefinitionMetadata();
+        configure?.Invoke(metadata);
+        _responseTypes.Add(responseType, new(typeof(TApiResponse), statusCode, metadata));
         return this;
     }
 }
@@ -31,5 +31,32 @@ public class ResponseTypeRegistry
 public record struct ResponseTypeDefinition
 (
     Type ApiResponseType,
-    int StatusCode
+    int StatusCode,
+    ResponseTypeDefinitionMetadata Metadata
 );
+
+public class ResponseTypeDefinitionMetadata
+{
+    readonly Dictionary<string, object?> _source = new();
+
+    public object? this[string name]
+    {
+        get
+        {
+            if (name is null || !_source.ContainsKey(name))
+                return null;
+
+            return _source[name];
+        }
+        set
+        {
+            if (name is null)
+                return;
+
+            if (_source.ContainsKey(name))
+                _source[name] = value;
+            else
+                _source.Add(name, value);
+        }
+    }
+}
