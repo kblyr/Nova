9aca963f02990273d9476c0c975f59bbc7790a05
John Josua Paderon <johnjosua.paderon@outlook.com>
2022-03-09 21:02:17 +0800

    UserStatusLookupKeys

---
+++ src/Identity/Core/UserStatusLookupKeys.cs
@@ -0,0 +1,8 @@
+namespace Nova.Identity;
+
+public static class UserStatusLookupKeys
+{
+    public const string Pending = "NOVA:IDENTITY:USER-STATUS:PENDING";
+    public const string Active = "NOVA:IDENTITY:USER-STATUS:ACTIVE";
+    public const string Locked = "NOVA:IDENTITY:USER-STATUS:LOCKED";
+}
