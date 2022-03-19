ea091e8a1ee4e9c6fca81fd8d196cf0a7f68f94c
John Josua Paderon <johnjosua.paderon@outlook.com>
2022-03-20 00:38:44 +0800

    Dependency Injection

--- src/HRIS/WebAPI/Program.cs
+++ src/HRIS/WebAPI/Program.cs
@@ -1,5 +1,14 @@
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Nova.Core;
+using Nova.Core.Utilities;
+using Nova.EFCore;
+using Nova.HRIS.Core;
+using Nova.HRIS.EFCore;
+using Nova.HRIS.EFCore.Postgres;
+using Nova.Web;
+using Nova.Web.Auditing;
+using Nova.Web.Messaging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,8 +24,24 @@ builder.Services
     .AddMediatR(assemblyMarkers)
     .AddAutoMapper(assemblyMarkers)
     .AddResponseMapping(Nova.HRIS.WebAPI.AssemblyMarker.Assembly);
+
+builder.Services.AddNova(nova => nova
+    .AddUtilities()
+    .EFCore(efCore => efCore
+        .AddDbContextFactory<Nova.HRIS.EmployeeDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres:Nova:HRIS")))
+    )
+    .Web(web => web
+        .AddAuditing()
+        .AddMessaging()
+    )
+    .HRIS(hris => hris
+        .EFCore(efCore => efCore
+            .Postgres(postgres => postgres.AddDefault())
+        )
+    )
+);
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.UseAuthentication();
+app.MapEndpoints(Nova.HRIS.WebAPI.AssemblyMarker.Assembly);
 app.Run();
