25bab2b9ea5b3651b79803b5e517f969cd2bd47d
John Josua Paderon <johnjosua.paderon@outlook.com>
2022-05-17 11:31:13 +0800

    UserEmailVerificationSaga

--- src/Identity/Messaging.Host/Program.cs
+++ src/Identity/Messaging.Host/Program.cs
@@ -5,9 +5,17 @@ var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services => {
         services.AddMassTransit(massTransit => {
             massTransit.AddSagaStateMachine<UserSaga, UserSaga.Instance>()
+                .Endpoint(config => config.Name = "Nova_Identity_User")
                 .MongoDbRepository("mongodb://mongo:mongo@localhost:27017/?authSource=admin&readPreference=primary", config => {
                     config.DatabaseName = "nova_identity";
                     config.CollectionName = "user";
+                });
+
+            massTransit.AddSagaStateMachine<UserEmailVerificationSaga, UserEmailVerificationSaga.Instance>()
+                .Endpoint(config => config.Name = "Nova_Identity_UserEmailVerification")
+                .MongoDbRepository("mongodb://mongo:mongo@localhost:27017/?authSource=admin&readPreference=primary", config => {
+                    config.DatabaseName = "nova_identity";
+                    config.CollectionName = "user_email_verification";
                 });
 
             massTransit.UsingRabbitMq((context, rabbitMq) => {
