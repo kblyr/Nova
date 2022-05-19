fd08375d26a7723386115c31620fd1ca43b2cf8c
John Josua Paderon <johnjosua.paderon@outlook.com>
2022-05-19 21:10:22 +0800

    Connection strings from config

--- src/Identity/Messaging.Host/Program.cs
+++ src/Identity/Messaging.Host/Program.cs
@@ -2,17 +2,17 @@ using MassTransit;
 using Nova.Sagas;
 
 var host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services => {
+    .ConfigureServices((host, services) => {
         services.AddMassTransit(massTransit => {
             massTransit.AddSagaStateMachine<EmailVerificationSaga, EmailVerificationSaga.Instance>()
                 .Endpoint(config => config.Name = "Nova_Identity_EmailVerification")
-                .MongoDbRepository("mongodb://mongo:mongo@localhost:27017/?authSource=admin&readPreference=primary", config => {
+                .MongoDbRepository(host.Configuration.GetConnectionString("MongoDB:Identity"), config => {
                     config.DatabaseName = "nova_identity";
                     config.CollectionName = "email_verification";
                 });
 
             massTransit.UsingRabbitMq((context, rabbitMq) => {
-                rabbitMq.Host("rabbitmq://localhost:5672");
+                rabbitMq.Host(host.Configuration.GetConnectionString("RabbitMQ:Identity"));
                 rabbitMq.ConfigureEndpoints(context);
             });
         });
