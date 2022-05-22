bf3214719a1c6830e51831d16ac1fea90ba2f4e5
John Josua Paderon <johnjosua.paderon@outlook.com>
2022-05-22 23:47:23 +0800

    feature: register UserEmailVerificationSaga

--- src/Identity/Messaging.Host/Program.cs
+++ src/Identity/Messaging.Host/Program.cs
@@ -4,11 +4,18 @@ using Nova.Identity.Sagas;
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((host, services) => {
         services.AddMassTransit(massTransit => {
+            var mongoDbConnectionString = host.Configuration["Nova:Identity:ConnectionStrings:MongoDB"];
             massTransit.AddSagaStateMachine<EmailVerificationSaga, EmailVerificationSaga.Instance>()
                 .Endpoint(config => config.Name = "Nova_Identity_EmailVerification")
-                .MongoDbRepository(host.Configuration["Nova:Identity:ConnectionStrings:MongoDB"], config => {
+                .MongoDbRepository(mongoDbConnectionString, config => {
                     config.DatabaseName = "nova_identity";
                     config.CollectionName = "email_verification";
+                });
+            massTransit.AddSagaStateMachine<UserEmailVerificationSaga, UserEmailVerificationSaga.Instance>()
+                .Endpoint(config => config.Name = "Nova_Identity_UserEmailVerification")
+                .MongoDbRepository(mongoDbConnectionString, config => {
+                    config.DatabaseName = "nova_identity";
+                    config.CollectionName = "user_email_verification";
                 });
 
             massTransit.UsingRabbitMq((context, rabbitMq) => {
