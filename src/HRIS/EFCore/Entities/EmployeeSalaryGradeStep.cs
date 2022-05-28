a10be55ab516b684c58f126b76aff0cf33654253
John Josua Paderon <johnjosua.paderon@outlook.com>
2022-05-28 20:29:00 +0800

    feature: Salary Grade entities

---
+++ src/HRIS/EFCore/Entities/EmployeeSalaryGradeStep.cs
@@ -0,0 +1,20 @@
+#nullable disable
+namespace Nova.HRIS.Entities;
+
+public class EmployeeSalaryGradeStep
+{
+    public long Id { get; set; }
+    public int EmployeeId { get; set; }
+    public short Grade { get; set; }
+    public short Step { get; set; }
+    public DateTimeOffset EffectivityBeginDate { get; set; }
+    public DateTimeOffset? EffectivityEndDate { get; set; }
+
+    public bool IsDeleted { get; set; }
+    public int? InsertedById { get; set; }
+    public DateTimeOffset? InsertedOn { get; set; }
+    public int? DeletedById { get; set; }
+    public DateTimeOffset? DeletedOn { get; set; }
+
+    public Employee Employee { get; set; }
+}
---
+++ src/HRIS/EFCore/Entities/SalaryGradeTable.cs
@@ -0,0 +1,18 @@
+#nullable disable
+namespace Nova.HRIS.Entities;
+
+public class SalaryGradeTable
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public DateTimeOffset EffectivityBeginDate { get; set; }
+    public DateTimeOffset? EffectivityEndDate { get; set; }
+
+    public bool IsDeleted { get; set; }
+    public int? InsertedById { get; set; }
+    public DateTimeOffset? InsertedOn { get; set; }
+    public int? DeletedById { get; set; }
+    public DateTimeOffset? DeletedOn { get; set; }
+
+    public IEnumerable<SalaryGradeStep> Steps { get; set; }
+}
